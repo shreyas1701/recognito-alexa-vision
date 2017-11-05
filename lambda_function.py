913088fecf7e7156ae1d5075a1c30ab8207a45bc
Sanket Patil <sanket.patil30@gmail.com>
2017-11-05 07:08:30 -0500

    lambda function hosted on aws lambda

---
+++ lambda_function.py
@@ -0,0 +1,141 @@
+import numpy as np
+import urllib
+import cv2
+import boto3
+import logging
+
+##############################
+# Builders
+##############################
+
+
+def build_PlainSpeech(body):
+    speech = {}
+    speech['type'] = 'PlainText'
+    speech['text'] = body
+    return speech
+
+
+def build_response(message, session_attributes={}):
+    response = {}
+    response['version'] = '1.0'
+    response['sessionAttributes'] = session_attributes
+    response['response'] = message
+    return response
+
+
+def build_SimpleCard(title, body):
+    card = {}
+    card['type'] = 'Simple'
+    card['title'] = title
+    card['content'] = body
+    return card
+
+
+##############################
+# Responses
+##############################
+
+
+def conversation(title, body, session_attributes):
+    speechlet = {}
+    speechlet['outputSpeech'] = build_PlainSpeech(body)
+    speechlet['card'] = build_SimpleCard(title, body)
+    speechlet['shouldEndSession'] = False
+    return build_response(speechlet, session_attributes=session_attributes)
+
+
+def statement(title, body):
+    speechlet = {}
+    speechlet['outputSpeech'] = build_PlainSpeech(body)
+    speechlet['card'] = build_SimpleCard(title, body)
+    speechlet['shouldEndSession'] = True
+    return build_response(speechlet)
+
+
+def continue_dialog():
+    message = {}
+    message['shouldEndSession'] = False
+    message['directives'] = [{'type': 'Dialog.Delegate'}]
+    return build_response(message)
+
+
+##############################
+# Custom Intents
+##############################
+
+def recognize_intent(event, context): 
+    #Call Rasberry Pi for image Capture
+    resp = urllib.urlopen('https://cdd77821.ngrok.io/capture')
+    #image = Image.open(BytesIO(response.read()))
+    #image = np.asarray(bytearray(response.read()), dtype="uint8")
+    #image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    key_id = 'AKIAJTHNHB5NPICLCHKQ'
+    secret_key = '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'
+    client = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id=key_id, aws_secret_access_key=secret_key)
+    response = client.detect_labels(Image={'Bytes': resp.read()})
+    return statement("", response)
+
+
+##############################
+# Required Intents
+##############################
+
+
+def cancel_intent():
+    return statement("CancelIntent", "You want to cancel")
+
+
+def help_intent():
+    return statement("CancelIntent", "You want help")
+
+
+def stop_intent():
+    return statement("StopIntent", "You want to stop")
+
+
+##############################
+# On Launch
+##############################
+
+
+def on_launch(event, context):
+    return statement("title", "body")
+
+
+##############################
+# Routing
+##############################
+
+
+def intent_router(event, context):
+    intent = event['request']['intent']['name']
+
+    # Custom Intents
+
+    if intent == "RecognizeIntent":
+        return recognize_intent(event, context)
+
+    # Required Intents
+
+    if intent == "AMAZON.CancelIntent":
+        return cancel_intent()
+
+    if intent == "AMAZON.HelpIntent":
+        return help_intent()
+
+    if intent == "AMAZON.StopIntent":
+        return stop_intent()
+
+
+##############################
+# Program Entry
+##############################
+
+
+def lambda_handler(event, context):
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event, context)
+
+    elif event['request']['type'] == "IntentRequest":
+        return intent_router(event, context)
